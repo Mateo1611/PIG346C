d47d4f80e646a5cf295fc8a0a7e8ddc6e36358d3
Mateo Yepes <mateoyepessierra@gmail.com>
2026-01-21 18:02:40 -0500

    Mi primer commit

---
+++ Mision1/Proyecto1/src/com/empresa/gestion/model/Persona.java
@@ -0,0 +1,36 @@
+package com.empresa.gestion.model;
+
+public class Persona {
+    //Encapsulación 
+    private String nombre;
+    private int edad;
+
+    //Constructor
+
+    public Persona(String nombre, int edad){
+        this.nombre=nombre;
+        this.edad=edad;
+
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+     public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+//Método
+    public void presentarse(){
+        System.out.println("Hola, soy"+nombre+" y tengo "+edad+" años");
+    }
+
+    
+}
